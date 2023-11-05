56bd86a08cff6f5e34e022adcad01089f93c6f53
jeremie-code <be.jeremie@gmail.comHamuda88git config --global user.name>
2023-11-05 10:37:34 +0200

    fix: General display and refactoring. * Add accent to first name. * fix mobile menu. * match typo to the type of the docx. * Refactor displayContent function. * Improve select language feauture. * Improve style.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Funciton to get data per languages from Json.
 function displayContent(language, config) {
+    displayContentDescription(language, config);
+    displayContentExperience(language, config);
+    displayContentSkills(language, config);
+    displayContentEducation(language, config);
+    displayContentLanguages(language, config);
+    displayContentHobbies(language, config);
+};
 
-    // Description sections.
+// Description sections.
+function displayContentDescription(language, config) {
     // document.getElementById('name').innerText = config.name[language];
     document.getElementById('intro').innerText = config.intro[language];
     document.getElementById('presentation').innerText = config.presentation[language];
+};
 
-    // Experience sections.
+// Experience sections.
+function displayContentExperience(language, config) {
     var experienceList = '';
     config.experience[language].forEach(function (exp) {
         experienceList += '<li class=""><strong>' + exp.position + '</strong> - ' + exp.duration + ' at ' + exp.company + '<ul>';
@@ -16,8 +26,10 @@ function displayContent(language, config) {
         experienceList += '</ul></li>';
     });
     document.getElementById('experienceList').innerHTML = experienceList;
+};
 
-    // Skills sections.
+// Skills sections.
+function displayContentSkills(language, config) {
     var skillsList = '';
     config.skills[language].forEach(function (ski) {
         skillsList += '<li class=""><strong>' + ski.knowlegde + '</strong> - ' + ski.level + '<ul>';
@@ -27,36 +39,44 @@ function displayContent(language, config) {
         skillsList += '</ul></li>';
     });
     document.getElementById('skillsList').innerHTML = skillsList;
+};
 
-    // Education sections.
+// Education sections.
+function displayContentEducation(language, config) {
     var educationList = '';
     config.education[language].forEach(function (edu) {
         educationList += '<li class=""><strong>' + edu.degree + '</strong>, ' + edu.duration + ' at ' + edu.university + '</li>';
     });
     document.getElementById('educationList').innerHTML = educationList;
+};
 
-    // Languages sections.
+// Languages sections.
+function displayContentLanguages(language, config) {
     var languageList = '';
     config.languages[language].forEach(function (lang) {
         languageList += '<li class=""><strong>' + lang.language + '</strong>: ' + lang.level + '</li>';
     });
     document.getElementById('languagesList').innerHTML = languageList;
+};
 
-    // Hobbies sections.
+// Hobbies sections.
+function displayContentHobbies(language, config) {
     var hobbiesList = '';
     config.hobbies[language].forEach(function (hobby) {
         hobbiesList += '<li class=""><strong>' + hobby.name + '</strong>: ' + hobby.description + '</li>';
     });
     document.getElementById('hobbiesList').innerHTML = hobbiesList;
-}
+};
 
 // Function to switch to French
 function switchToFrench() {
+    fetch('./data.json')
+        .then(response => response.json())
+        .then(data => displayContent('fr', data));
+    unSelectElement("flagEn");
+
     alert("Bientot disponible, Travaux en cours!" + "\n" + "Soon available, work in progress!");
     switchToEnglish();
-    // fetch('./data.json')
-    //     .then(response => response.json())
-    //     .then(data => displayContent('fr', data));
 }
 
 // Function to switch to English
@@ -64,9 +84,17 @@ function switchToEnglish() {
     fetch('./data.json')
         .then(response => response.json())
         .then(data => displayContent('en', data));
+    unSelectElement("flagFr");
+}
+
+function unSelectElement(id) {
+    const elements = document.querySelectorAll('.unselected');
+    elements.forEach(element => {
+        element.classList.remove('unselected');
+    });
+    elementToSelect = document.getElementById(id);
+    elementToSelect.classList.add('unselected');
 }
 
 // Display content in English by default
-fetch('./data.json')
-    .then(response => response.json())
-    .then(data => displayContent('en', data));
+switchToEnglish();
